dd67a284661ee37f9909d227da37e949b63a8676
fikiaprian23 <fikiaprian23@gmail.com>
2023-11-19 22:57:14 +0700

    update auth

--- frontend/src/components/Auth/LoginCard.jsx
+++ frontend/src/components/Auth/LoginCard.jsx
@@ -112,7 +112,7 @@ export default function LoginCard() {
               <Text color={'blue.500'}>Forgot password?</Text>
             </Stack>
             <Button
-              colorScheme={'blue'}
+              colorScheme={'teal'}
               variant={'solid'}
               onClick={handleLogin}
               isLoading={loading}
@@ -139,9 +139,7 @@ export default function LoginCard() {
         <Image
           alt={'Login Image'}
           objectFit={'cover'}
-          src={
-            'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
-          }
+          src={'/Research paper-bro.png'}
           h="full"
         />
       </Box>
--- frontend/src/components/Auth/SignupCard.jsx
+++ frontend/src/components/Auth/SignupCard.jsx
@@ -174,9 +174,7 @@ export default function SignupCard() {
         <Image
           alt={'Login Image'}
           objectFit={'cover'}
-          src={
-            'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
-          }
+          src={'/Research paper-rafiki.png'}
           h="full"
         />
       </Box>
